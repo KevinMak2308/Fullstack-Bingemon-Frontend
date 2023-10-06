8c1e140d789b7d9fd3dfb62de499fff9b3551059
“isab554b” <isab554b@stud.kea.dk>
2023-10-06 10:35:22 +0200

    if statement

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar"
+import NavBarLoggedIn from "./components/NavBarLoggedIn"
 import SubNav from "./components/SubNav"
+import SubNavLoggedIn from "./components/SubNavLoggedIn"
 import SignupModal from "./components/SignupModal"
 
 function App() {
-    return (
+
+    if (localStorage.getItem("user")) {
+        return (
         <div className="App">
+            <NavBarLoggedIn/>
+            <SubNavLoggedIn/>
+        </div>
+        );
+    }
+
+    else {
+        return (
+        <div>
             <NavBar/>
             <SubNav/>
-            <SignupModal/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Our Frontend Typescript React Project for Bingemon - Delta Group
-                </a>
-            </header>
         </div>
-    );
+        );
+    }
 }
-
 export default App;
