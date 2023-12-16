2ef242bfb2cc2bf230ef8a183076f0e9d2df9c2f
frederikbraad <frederik.holm.braad@hotmail.dk>
2023-12-16 17:09:21 +0100

    Testing react scripts and mocking ChakraUI
    fixing jest test error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// Mock Chakra UI's useBreakpointValue
+jest.mock('@chakra-ui/react', () => ({
+  ...jest.requireActual('@chakra-ui/react'),
+  useBreakpointValue: jest.fn().mockReturnValue("125px"), // Mock return value
+}));
 
 test('renders learn react link', () => {
   render(<App />);
